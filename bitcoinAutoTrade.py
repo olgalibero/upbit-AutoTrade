2e882478a5186f49b840b8d5ac0ae6a20dc0b175
olgalibero <59647492+olgalibero@users.noreply.github.com>
2021-04-26 23:48:46 +0900

    Update bitcoinAutoTrade.py
    xrp
    slack
    start time
    limited krw

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -1,9 +1,29 @@
+from slacker import Slacker
 import time
 import pyupbit
 import datetime
+import requests
 
 access = "your-access"
 secret = "your-secret"
+
+
+def post_message(token, channel, text):
+    response = requests.post("https://slack.com/api/chat.postMessage",
+                             headers={"Authorization": "Bearer "+token},
+                             data={"channel": channel, "text": text}
+                             )
+    print(response)
+
+
+myToken = "xoxb-2007319949188-2001383495923-77zp4qFhQWzVjWVTcYXwE8jE"
+
+
+def dbgout(message):
+    """인자로 받은 문자열을 파이썬 셸과 슬랙으로 동시에 출력한다."""
+    print(datetime.now().strftime('[%m/%d %H:%M:%S]'), message)
+    strbuf = datetime.now().strftime('[%m/%d %H:%M:%S] ') + message
+    post_message(myToken, "#project", strbuf)
 
 
 def get_target_price(ticker, k):
@@ -40,26 +60,33 @@ def get_current_price(ticker):
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
+# Send a message to #general channel
+slack.chat.post_message("#project", "autotrade start")
+
+flag = True
 
 # 자동매매 시작
 while True:
+    if (flag):
+    upbit.buy_market_order("KRW-XRP", 3000000)
+    flag = False
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-BTC")
-        end_time = start_time + datetime.timedelta(days=1)
+        start_time = get_start_time("KRW-XRP") + datetime.timedelta(hours=6)
+        end_time = start_time + datetime.timedelta(hours=5, minutes=55)
 
 # 9:00 < 현재 < #8:59:50
-        if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.5)
-            current_price = get_current_price("KRW-BTC")
+        if start_time < now < end_time - datetime.timedelta(seconds=60):
+            target_price = get_target_price("KRW-XRP", 0.55)
+            current_price = get_current_price("KRW-XRP")
             if target_price < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                    upbit.buy_market_order("KRW-XRP", krw*0.9995)
         else:
-            btc = get_balance("BTC")
-            if btc > 0.00008:
-                upbit.sell_market_order("KRW-BTC", btc*0.9995)
+            xrp = get_balance("XRP")
+            if xrp > 4.5:
+                upbit.sell_market_order("KRW-XRP", xrp*0.9995)
             time.sleep(1)
     except Exception as e:
         print(e)
