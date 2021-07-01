bb128a1e1681ba7861961ab7c5b70d6e98c1a86f
olgalibero <59647492+olgalibero@users.noreply.github.com>
2021-07-02 00:50:12 +0900

    Update bitcoinAutoTrade.py
    btc ma5 
    sell 8:30

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -1,29 +1,9 @@
-from slacker import Slacker
 import time
 import pyupbit
 import datetime
-import requests
 
-access = "your-access"
-secret = "your-secret"
-
-
-def post_message(token, channel, text):
-    response = requests.post("https://slack.com/api/chat.postMessage",
-                             headers={"Authorization": "Bearer "+token},
-                             data={"channel": channel, "text": text}
-                             )
-    print(response)
-
-
-myToken = "xoxb-2007319949188-2001383495923-77zp4qFhQWzVjWVTcYXwE8jE"
-
-
-def dbgout(message):
-    """인자로 받은 문자열을 파이썬 셸과 슬랙으로 동시에 출력한다."""
-    print(datetime.now().strftime('[%m/%d %H:%M:%S]'), message)
-    strbuf = datetime.now().strftime('[%m/%d %H:%M:%S] ') + message
-    post_message(myToken, "#project", strbuf)
+access = "hbBdBOjDeWPzGeBARxIsJ8bYyQb98ZmAU3yGHYzo"
+secret = "oyxIjxLmeH1uxfXJq7xKD8DcSwzBxfaEa8NNHKtw"
 
 
 def get_target_price(ticker, k):
@@ -41,6 +21,13 @@ def get_start_time(ticker):
     return start_time
 
 
+def get_ma5(ticker):
+    """15일 이동 평균선 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=5)
+    ma5 = df['close'].rolling(5).mean().iloc[-1]
+    return ma5
+
+
 def get_balance(ticker):
     """잔고 조회"""
     balances = upbit.get_balances()
@@ -50,6 +37,7 @@ def get_balance(ticker):
                 return float(b['balance'])
             else:
                 return 0
+    return 0
 
 
 def get_current_price(ticker):
@@ -60,34 +48,26 @@ def get_current_price(ticker):
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
-# Send a message to #general channel
-slack.chat.post_message("#project", "autotrade start")
-
-flag = True
 
 # 자동매매 시작
 while True:
-    if (flag):
-    upbit.buy_market_order("KRW-XRP", 3000000)
-    flag = False
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-XRP") + datetime.timedelta(hours=6)
-        end_time = start_time + datetime.timedelta(hours=5, minutes=55)
+        start_time = get_start_time("KRW-BTC")
+        end_time = start_time + datetime.timedelta(days=1)
 
-# 9:00 < 현재 < #8:59:50
-        if start_time < now < end_time - datetime.timedelta(seconds=60):
-            target_price = get_target_price("KRW-XRP", 0.55)
-            current_price = get_current_price("KRW-XRP")
+        if start_time < now < end_time - datetime.timedelta(seconds=1800):
+            target_price = get_target_price("KRW-BTC", 0.65)
+            current_price = get_current_price("KRW-BTC")
             if target_price < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    upbit.buy_market_order("KRW-XRP", krw*0.9995)
+                    upbit.buy_market_order("KRW-BTC", krw*0.9995)
         else:
-            xrp = get_balance("XRP")
-            if xrp > 4.5:
-                upbit.sell_market_order("KRW-XRP", xrp*0.9995)
-            time.sleep(1)
+            btc = get_balance("BTC")
+            if btc > 0.00008:
+                upbit.sell_market_order("KRW-BTC", btc*0.9995)
+        time.sleep(1)
     except Exception as e:
         print(e)
         time.sleep(1)
